d56ca9cca46335794d7372ecf24d91c42956977e
vivek865 <kumarvivek865@gmail.com>
2016-06-27 12:20:30 +0530

    Create walker-class.php

---
+++ walker-class.php
@@ -0,0 +1,38 @@
+// If item has_children add atts to a.
+			if ( $args->has_children && $depth === 0 ) {
+				$atts['href']   		= '#';
+				$atts['data-toggle']	= 'dropdown';
+				$atts['class']			= 'dropdown-toggle';
+				$atts['aria-haspopup']	= 'true';
+			} else {
+				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+			}
+repace the above code with this code in walker class
+if ( $args->has_children && $depth === 0 ) {
+				 $atts['href'] = ! empty( $item->url ) ? $item->url : '';
+				$atts['data-hover']	= 'dropdown';
+				$atts['class']	= 'dropdown-toggle disabled ';
+				$atts['aria-haspopup']	= 'true';
+			} else {
+				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+			}
+
+
+
+<!--java script to show mwnu on hover the mouse -->
+<script>
+ $(function(){
+    $(".dropdown").hover(            
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            },
+            function() {
+                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+                $(this).toggleClass('open');
+                $('b', this).toggleClass("caret caret-up");                
+            });
+    });
+    
+</script>
